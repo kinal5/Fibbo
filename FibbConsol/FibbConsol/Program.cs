e7452ff0814ea018e7fd0e070ac88407e3f12888
kinal5 <monika.cinal@gmail.com>
2015-04-20 11:43:13 +0200

    zmiany
    zmiany

---
+++ FibbConsol/FibbConsol/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibbConsol
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Check value if is even or not
+        /// 
+        /// Example:
+        /// 
+        /// EvenValue(8)
+        ///     #=>true
+        ///     
+        /// EvenValue(13)
+        ///     #=>false
+        /// 
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns> Return true when value is even number and false when is not </returns>
+        public static Boolean EvenValue(uint number)
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        public static uint Fibonacci(uint n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(n - 2) + Fibonacci(n - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            uint i = 2;
+            uint suma = 0;
+            while (Fibonacci(i) < 4000000)
+            {
+                if(EvenValue(Fibonacci(i)) == true)
+                {
+                    suma += Fibonacci(i);
+                }
+                i++;
+            }
+            Console.WriteLine(suma);
+            Console.ReadKey();
+        }
+    }
+}
